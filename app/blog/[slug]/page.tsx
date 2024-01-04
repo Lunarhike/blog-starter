0278da65de40b2bb28e7ffc0239e7efb77b9f8c8
lunarhike <lunarhike@gmail.com>
2024-01-04 16:52:21 +0100

    test of the second approach

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,28 +1,80 @@
-import { notFound } from "next/navigation";
-import { Mdx } from "@/components/mdx";
-import { getBlogPosts } from "@/lib/blog";
+// import { notFound } from "next/navigation";
+// import { Mdx } from "@/components/mdx";
+// import { getBlogPosts } from "@/lib/blog";
+// import rehypePrettyCode from "rehype-pretty-code";
+
+// export default function Blog({ params }) {
+//   let post = getBlogPosts().find((post) => post.slug === params.slug);
+
+//   if (!post) {
+//     notFound();
+//   }
+
+//   return (
+//     <section>
+//       <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+//         {post.metadata.title}
+//       </h1>
+//       <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+//         <p className="text-sm text-neutral-600 dark:text-neutral-400">
+//           {post.metadata.publishedAt}
+//         </p>
+//       </div>
+//       <article className="">
+//         <Mdx
+//           source={post.content}
+//           options={{
+//             mdxOptions: {
+//               useDynamicImport: true,
+//               rehypePlugins: [
+//                 [
+//                   rehypePrettyCode,
+//                   {
+//                     theme: "slack-dark",
+//                   },
+//                 ],
+//               ],
+//             },
+//           }}
+//         />
+//       </article>
+//     </section>
+//   );
+// }
+
+import { readdir, readFile } from "fs/promises";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypePrettyCode from "rehype-pretty-code";
+import "@/styles/mdx.css";
+import { Mdx } from "@/components/mdx";
 
-export default function Blog({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug);
+export default async function PostPage({ params }) {
+  const filename = "./content/" + params.slug + ".mdx";
+  const file = await readFile(filename, "utf8");
+  let postComponents = {};
 
-  if (!post) {
-    notFound();
-  }
+  const { content, data } = matter(file);
 
   return (
-    <section>
-      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
-        {post.metadata.title}
+    <article>
+      <h1
+        className={[
+          "text-[40px] font-black leading-[44px] text-[--title]",
+        ].join(" ")}
+      >
+        {data.title}
       </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {post.metadata.publishedAt}
-        </p>
-      </div>
-      <article className="">
+      <p className="mt-2 text-[13px] text-gray-700 dark:text-gray-300">
+        {new Date(data.date).toLocaleDateString("en", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })}
+      </p>
+      <div className="markdown mt-10">
         <Mdx
-          source={post.content}
+          source={content}
           options={{
             mdxOptions: {
               useDynamicImport: true,
@@ -37,71 +89,14 @@ export default function Blog({ params }) {
             },
           }}
         />
-      </article>
-    </section>
+        <hr />
+      </div>
+    </article>
   );
 }
 
-// import { readdir, readFile } from "fs/promises";
-// import matter from "gray-matter";
-// import { MDXRemote } from "next-mdx-remote/rsc";
-// import rehypePrettyCode from "rehype-pretty-code";
-// import "@/styles/mdx.css";
-
-// export default async function PostPage({ params }) {
-//   const filename = "./content/" + params.slug + ".mdx";
-//   const file = await readFile(filename, "utf8");
-//   let postComponents = {};
-
-//   const { content, data } = matter(file);
-
-//   return (
-//     <article>
-//       <h1
-//         className={[
-//           "text-[40px] font-black leading-[44px] text-[--title]",
-//         ].join(" ")}
-//       >
-//         {data.title}
-//       </h1>
-//       <p className="mt-2 text-[13px] text-gray-700 dark:text-gray-300">
-//         {new Date(data.date).toLocaleDateString("en", {
-//           day: "numeric",
-//           month: "long",
-//           year: "numeric",
-//         })}
-//       </p>
-//       <div className="markdown mt-10">
-//         <MDXRemote
-//           source={content}
-//           components={{
-//             Callout: ({ children, className }) => (
-//               <div className={className}>{children}</div>
-//             ),
-//             ...postComponents,
-//           }}
-//           options={{
-//             mdxOptions: {
-//               useDynamicImport: true,
-//               rehypePlugins: [
-//                 [
-//                   rehypePrettyCode,
-//                   {
-//                     theme: "slack-ochin",
-//                   },
-//                 ],
-//               ],
-//             },
-//           }}
-//         />
-//         <hr />
-//       </div>
-//     </article>
-//   );
-// }
-
-// export async function generateStaticParams() {
-//   const entries = await readdir("./content/", { withFileTypes: true });
-//   const dirs = entries.map((entry) => entry.name);
-//   return dirs.map((dir) => ({ slug: dir }));
-// }
+export async function generateStaticParams() {
+  const entries = await readdir("./content/", { withFileTypes: true });
+  const dirs = entries.map((entry) => entry.name);
+  return dirs.map((dir) => ({ slug: dir }));
+}
