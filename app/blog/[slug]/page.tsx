76559b8a645b2b7138989e5663f7652a0686e3a9
lunarhike <lunarhike@gmail.com>
2024-01-06 21:18:06 +0100

    changed authors section, added suspense to image

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,6 +7,7 @@ import path from "path";
 import Image from "next/image";
 import Link from "next/link";
 import { formatDate } from "@/lib/utils";
+import { Suspense } from "react";
 
 async function getAuthorDetails(authorsList) {
   if (!authorsList || authorsList.length === 0) {
@@ -57,34 +58,32 @@ export default async function PostPage({ params }) {
         <h1 className="mt-2 font-heading font-bold inline-block text-4xl leading-tight tracking-tight lg:text-5xl ">
           {data.title}
         </h1>
-        {authorsInfo && (
-          <div className="mt-4 flex space-x-4">
-            {authorsInfo.map((author) =>
-              author ? (
-                <Link
-                  key={author.name}
-                  href={`https://www.linkedin.com/in/${author.linkedIn}`}
-                  className="flex items-center space-x-2 text-sm"
-                >
-                  <Image
-                    placeholder="blur"
-                    src={author.avatar}
-                    alt={author.name}
-                    width={42}
-                    height={42}
-                    className="rounded-full"
-                  />
-                  <div className="flex-1 text-left leading-tight">
-                    <p className="font-medium">{author.name}</p>
-                    <p className="text-[12px] text-muted-foreground">
-                      @LinkedIn
-                    </p>
-                  </div>
-                </Link>
-              ) : null
-            )}
-          </div>
-        )}
+        <div className="mt-4 flex space-x-4">
+          <Suspense
+            fallback={<div className="w-96 h-96 bg-red-500">siema</div>}
+          >
+            {authorsInfo.map((author) => (
+              <Link
+                key={author.name}
+                href={`https://www.linkedin.com/in/${author.linkedIn}`}
+                className="flex items-center space-x-2 text-sm"
+              >
+                <Image
+                  loading="lazy"
+                  src={author.avatar}
+                  alt={author.name}
+                  width={42}
+                  height={42}
+                  className="rounded-full"
+                />
+                <div className="flex-1 text-left leading-tight">
+                  <p className="font-medium">{author.name}</p>
+                  <p className="text-[12px] text-muted-foreground">@LinkedIn</p>
+                </div>
+              </Link>
+            ))}
+          </Suspense>
+        </div>
       </div>
       {data.image && (
         <Image
