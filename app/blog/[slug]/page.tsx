d6fc562dafa211a9d1070bd0c1083661e948aed2
lunarhike <lunarhike@gmail.com>
2024-01-06 22:36:46 +0100

    placeholder test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,25 +8,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { formatDate } from "@/lib/utils";
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
-
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
-
 async function getAuthorDetails(authorsList) {
   // If authorsList is empty or not provided, return an empty array
   if (!authorsList || authorsList.length === 0) {
@@ -107,9 +88,6 @@ export default async function PostPage({ params }) {
           alt={data.title}
           width={650}
           height={650}
-          placeholder={`data:image/svg+xml;base64,${toBase64(
-            shimmer(650, 650)
-          )}`}
           className="my-8 rounded-md border bg-zinc-500 transition-colors"
           priority
           loading="lazy"
