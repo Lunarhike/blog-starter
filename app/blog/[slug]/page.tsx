1c65053863ea1f6560379b30532a73e89ea7b966
lunarhike <lunarhike@gmail.com>
2024-01-06 21:27:52 +0100

    case sensitivity author test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,9 +7,9 @@ import path from "path";
 import Image from "next/image";
 import Link from "next/link";
 import { formatDate } from "@/lib/utils";
-import { Suspense } from "react";
 
 async function getAuthorDetails(authorsList) {
+  // If authorsList is empty or not provided, return an empty array
   if (!authorsList || authorsList.length === 0) {
     return [];
   }
@@ -17,7 +17,8 @@ async function getAuthorDetails(authorsList) {
   let authorsDetails = [];
 
   for (const authorName of authorsList) {
-    const authorFilename = `./content/authors/${authorName}.mdx`;
+    // Construct the path to the author's metadata file
+    const authorFilename = `./content/authors/${authorName.toLowerCase()}.mdx`; // Ensure the path is lowercase
 
     try {
       // Read the author's metadata file
@@ -59,30 +60,26 @@ export default async function PostPage({ params }) {
           {data.title}
         </h1>
         <div className="mt-4 flex space-x-4">
-          <Suspense
-            fallback={<div className="w-96 h-96 bg-red-500">siema</div>}
-          >
-            {authorsInfo.map((author) => (
-              <Link
-                key={author.name}
-                href={`https://www.linkedin.com/in/${author.linkedIn}`}
-                className="flex items-center space-x-2 text-sm"
-              >
-                <Image
-                  loading="lazy"
-                  src={author.avatar}
-                  alt={author.name}
-                  width={42}
-                  height={42}
-                  className="rounded-full"
-                />
-                <div className="flex-1 text-left leading-tight">
-                  <p className="font-medium">{author.name}</p>
-                  <p className="text-[12px] text-muted-foreground">@LinkedIn</p>
-                </div>
-              </Link>
-            ))}
-          </Suspense>
+          {authorsInfo.map((author) => (
+            <Link
+              key={author.name}
+              href={`https://www.linkedin.com/in/${author.linkedIn}`}
+              className="flex items-center space-x-2 text-sm"
+            >
+              <Image
+                loading="lazy"
+                src={author.avatar}
+                alt={author.name}
+                width={42}
+                height={42}
+                className="rounded-full"
+              />
+              <div className="flex-1 text-left leading-tight">
+                <p className="font-medium">{author.name}</p>
+                <p className="text-[12px] text-muted-foreground">@LinkedIn</p>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
       {data.image && (
